d4344cccf8b32adbab5b9b95c0bd0dd3825be9e7
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2024-03-10 16:29:05 +0200

    created ButtonGroup

---
+++ src/app/test/button-group/page.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+import ButtonGroupPage from '@/components/screen/test-page/button-group-page';
+
+const Page: FC = () => {
+  return <ButtonGroupPage />;
+};
+
+export default Page;
